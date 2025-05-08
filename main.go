06005adad4cb58209e14e4dace58dd6adc1d5180
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-08 15:13:56 +1000

    refactor: Remove cfg parameter from InitMongo and streamline config loading in main

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    
-    "fitness-tracker/internal/config"
-    "fitness-tracker/internal/database"
+	"log"
+	"net/http"
+
+    "fitness-tracker/internal/login"
+	"fitness-tracker/internal/config"
     "fitness-tracker/internal/routes"
+	"fitness-tracker/internal/database"
 )
 
 func main() {
     log.Println("Loding app config...")
-    cfg := config.LoadConfig()
+    config.LoadConfig()
+
+    log.Println("Initialising OIDC...")
+    login.InitOIDC()
 
     log.Println("Initialising database connection...")
-    database.InitMongo(cfg)
+    database.InitMongo()
 
     log.Println("Registering routes and multiplexer...")
     mux := http.NewServeMux()
     routes.RegisterRoutes(mux)
 
-    log.Printf("🚀 Server running on :%s", cfg.Port)
-    log.Fatal(http.ListenAndServe(":"+cfg.Port, mux))
+    log.Printf("Server running on :%s", config.AppConfig.Port)
+    log.Fatal(http.ListenAndServe(":" + config.AppConfig.Port, mux))
 }
