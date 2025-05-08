8f2acf0a7c87f52e354554b311a5e8a3125fc311
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-08 15:12:59 +1000

    feat: Organize route handlers by grouping user, routine, workout, session, and login endpoints

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,11 +8,26 @@ import (
 
 func RegisterRoutes(mux *http.ServeMux) {
 
+    // USER Handlers
     mux.HandleFunc("/user/id", handlers.GetUserHandler)
-    mux.HandleFunc("/session/data",handlers.GetSessionHandler)
+    mux.HandleFunc("/user/create", handlers.CreateUserHandler)
+    mux.HandleFunc("/user/update", handlers.UpdateUserHandler)
+
+    // ROUTINE Handlers
     mux.HandleFunc("/routines/list", handlers.GetRoutineListHandler)
     mux.HandleFunc("/routines/data", handlers.GetRoutineDataHandler)
+    mux.HandleFunc("/routines/delete", handlers.DeleteRoutineHandler)
+    mux.HandleFunc("/routines/update", handlers.UpdateRoutineHandler)
+
+    // WORKOUT Handlers
     mux.HandleFunc("/workouts/list", handlers.GetWorkoutListHandler)
     mux.HandleFunc("/workouts/data", handlers.GetWorkoutDataHandler)
+
+    // SESSION Handlers
+    mux.HandleFunc("/session/data", handlers.GetSessionHandler)
+
+    // LOGIN Handlers
+    mux.HandleFunc("/login", handlers.HandleLogin)
+    mux.HandleFunc("/callback", handlers.HandleCallback)
     
 }
