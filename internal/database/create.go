973c4e1237572c6301b61dd3ed008259e62ceb12
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-09 14:08:43 +1000

    feat: Implement CreateUser function for user creation in the database

--- internal/database/create.go
+++ internal/database/create.go
@@ -1,5 +1,27 @@
 package database
 
-func AddUser() string{
-	return "nil"
+import (
+	"context"
+	"log"
+	"time"
+
+	"fitness-tracker/internal/models"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func CreateUser(user models.User) (userID primitive.ObjectID, err error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    collection := GetCollection("users")
+
+    result, err := collection.InsertOne(ctx, user)
+    if err != nil {
+        log.Println("Couldn't create user")
+        return
+    }
+
+    userID = result.InsertedID.(primitive.ObjectID)
+    return
 }
