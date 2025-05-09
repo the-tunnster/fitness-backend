13c9481afdd4428de4a9396765f90454285e8246
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-09 14:13:58 +1000

    refactor: Improve error handling and consistency in DeleteSession and DeleteRoutine functions

--- internal/database/delete.go
+++ internal/database/delete.go
@@ -1,19 +1,45 @@
 package database
 
 import (
-	"time"
 	"context"
+	"errors"
+	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func DeleteSession(userId primitive.ObjectID) error {
+func DeleteSession(userID primitive.ObjectID) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	collection := GetCollection("sessions")
 
-	_, err := collection.DeleteOne(ctx, bson.M{"userId": userId})
-	return err
+	_, err = collection.DeleteOne(ctx, bson.M{"userId": userID})
+	return
+}
+
+func DeleteRoutine(userID, routiuneID primitive.ObjectID) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	collection := GetCollection("routines")
+
+	result, err := collection.DeleteOne(ctx, bson.M{
+		"_id":    routiuneID,
+		"userId": userID,
+	})
+
+	if err != nil {
+		log.Println("Failed to delete routine")
+		return err
+	}
+
+	if result.DeletedCount == 0 {
+		log.Println("Coudn't find specified routine")
+		return errors.New("no document with matching routineID and userID")
+	}
+
+	return
 }
