d3e85989a47386a79be78d5e8c25a72ce8699c9d
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-14 15:52:32 +1000

    style: Reformat code for consistency and readability in main.go

--- main.go
+++ main.go
@@ -4,26 +4,26 @@ import (
 	"log"
 	"net/http"
 
-    "fitness-tracker/internal/login"
 	"fitness-tracker/internal/config"
-    "fitness-tracker/internal/routes"
 	"fitness-tracker/internal/database"
+	"fitness-tracker/internal/login"
+	"fitness-tracker/internal/routes"
 )
 
 func main() {
-    log.Println("Loding app config...")
-    config.LoadConfig()
+	log.Println("Loding app config...")
+	config.LoadConfig()
 
-    log.Println("Initialising OIDC...")
-    login.InitOIDC()
+	log.Println("Initialising OIDC...")
+	login.InitOIDC()
 
-    log.Println("Initialising database connection...")
-    database.InitMongo()
+	log.Println("Initialising database connection...")
+	database.InitMongo()
 
-    log.Println("Registering routes and multiplexer...")
-    mux := http.NewServeMux()
-    routes.RegisterRoutes(mux)
+	log.Println("Registering routes and multiplexer...")
+	mux := http.NewServeMux()
+	routes.RegisterRoutes(mux)
 
-    log.Printf("Server running on :%s", config.AppConfig.Port)
-    log.Fatal(http.ListenAndServe(":" + config.AppConfig.Port, mux))
+	log.Printf("Server running on :%s", config.AppConfig.Port)
+	log.Fatal(http.ListenAndServe(":"+config.AppConfig.Port, mux))
 }
