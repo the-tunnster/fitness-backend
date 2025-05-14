c434eba512a36e9f473e1f648e3535bf42b0c02b
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-14 15:52:40 +1000

    feat: Add CreateExercise, CreateRoutine, CreateWorkout, and CreateSession functions for handling new entities

--- internal/database/create.go
+++ internal/database/create.go
@@ -25,3 +25,67 @@ func CreateUser(user models.User) (userID primitive.ObjectID, err error) {
     userID = result.InsertedID.(primitive.ObjectID)
     return
 }
+
+func CreateExercise(exercise models.Exercise) (exerciseID primitive.ObjectID, err error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    collection := GetCollection("exercises")
+
+    result, err := collection.InsertOne(ctx, exercise)
+    if err != nil {
+        log.Println("Couldn't create exercise")
+        return
+    }
+
+    exerciseID = result.InsertedID.(primitive.ObjectID)
+    return
+}
+
+func CreateRoutine(routine models.FullRoutine) (routineID primitive.ObjectID, err error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    collection := GetCollection("routines")
+
+    result, err := collection.InsertOne(ctx, routine)
+    if err != nil {
+        log.Println("Couldn't create routine")
+        return
+    }
+
+    routineID = result.InsertedID.(primitive.ObjectID)
+    return
+}
+
+func CreateWorkout(workout models.FullWorkout) (workoutID primitive.ObjectID, err error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    collection := GetCollection("workouts")
+
+    result, err := collection.InsertOne(ctx, workout)
+    if err != nil {
+        log.Println("Couldn't create workout")
+        return
+    }
+
+    workoutID = result.InsertedID.(primitive.ObjectID)
+    return
+}
+
+func CreateSession(session models.Session) (sessionID primitive.ObjectID, err error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    collection := GetCollection("sessions")
+
+    result, err := collection.InsertOne(ctx, session)
+    if err != nil {
+        log.Println("Couldn't create workout")
+        return
+    }
+
+    sessionID = result.InsertedID.(primitive.ObjectID)
+    return
+}
