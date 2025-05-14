b032834246e9c4c40fcc4b132a38b408af741369
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-14 15:55:24 +1000

    feat: Implement CreateExerciseHandler, CreateRoutineHandler, and CreateWorkoutHandler for user input processing

--- internal/handlers/post_handlers.go
+++ internal/handlers/post_handlers.go
@@ -25,3 +25,57 @@ func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(userID.Hex())
 }
+
+func CreateExerciseHandler(w http.ResponseWriter, r *http.Request) {
+	var exercise models.Exercise
+
+	if err := json.NewDecoder(r.Body).Decode(&exercise); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	exerciseID, err := database.CreateExercise(exercise)
+	if err != nil {
+		http.Error(w, "Failed to create exercise", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(exerciseID.Hex())
+}
+
+func CreateRoutineHandler(w http.ResponseWriter, r *http.Request) {
+	var routine models.FullRoutine
+
+	if err := json.NewDecoder(r.Body).Decode(&routine); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	routineID, err := database.CreateRoutine(routine)
+	if err != nil {
+		http.Error(w, "Failed to create routine", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(routineID.Hex())
+}
+
+func CreateWorkoutHandler(w http.ResponseWriter, r *http.Request) {
+	var workout models.FullWorkout
+
+		if err := json.NewDecoder(r.Body).Decode(&workout); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	workoutID, err := database.CreateWorkout(workout)
+	if err != nil {
+		http.Error(w, "Failed to create workout", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(workoutID.Hex())
+}
