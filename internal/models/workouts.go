4abd85c653b1af619e1e052273a0f196493440f2
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-14 15:56:21 +1000

    fix: Standardize field names from userId and routineId to userID and routineID in Workout and FullWorkout models feat: Add login handlers for OAuth2 authentication with callback processing feat: Register new workout and session routes in the router

--- internal/models/workouts.go
+++ internal/models/workouts.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Workout struct {
     ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-    UserID     primitive.ObjectID `bson:"userId" json:"user_id"`
-    RoutineID  primitive.ObjectID `bson:"routineId" json:"routine_id"`
+    UserID     primitive.ObjectID `bson:"userID" json:"user_id"`
+    RoutineID  primitive.ObjectID `bson:"routineID" json:"routine_id"`
     WorkoutDate primitive.DateTime `bson:"workoutDate" json:"workout_date"`
 }
 
@@ -16,14 +16,14 @@ type WorkoutSet struct {
 }
 
 type WorkoutExercise struct {
-    ExerciseID primitive.ObjectID `bson:"exerciseId" json:"exercise_id"`
+    ExerciseID primitive.ObjectID `bson:"exerciseID" json:"exercise_id"`
     Sets       []WorkoutSet       `bson:"sets" json:"sets"`
 }
 
 type FullWorkout struct {
     ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-    UserID      primitive.ObjectID `bson:"userId" json:"user_id"`
-    RoutineID   primitive.ObjectID `bson:"routineId" json:"routine_id"`
+    UserID      primitive.ObjectID `bson:"userID" json:"user_id"`
+    RoutineID   primitive.ObjectID `bson:"routineID" json:"routine_id"`
     WorkoutDate primitive.DateTime `bson:"workoutDate" json:"workout_date"`
     Exercises   []WorkoutExercise  `bson:"exercises" json:"exercises"`
 }
