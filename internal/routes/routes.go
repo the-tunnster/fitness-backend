09fdd0bb0940ef7e400906369a8dc839759ebb40
the-tunnster <therealtarunkrishnan@gmail.com>
2025-05-29 17:39:33 +1000

    refactor: Update route handlers to use middleware for improved security

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,37 +4,39 @@ import (
     "net/http"
 
     "fitness-tracker/internal/handlers"
+    "fitness-tracker/internal/middleware"
 )
 
 func RegisterRoutes(mux *http.ServeMux) {
 
-    // USER Handlers
-    mux.HandleFunc("/user/id", handlers.GetUserHandler)
-    mux.HandleFunc("/user/create", handlers.CreateUserHandler)
-    mux.HandleFunc("/user/update", handlers.UpdateUserHandler)
-
-    // EXERCISE Handlers
-    mux.HandleFunc("/exercise/create", handlers.CreateExerciseHandler)
-    mux.HandleFunc("/exercise/update", handlers.UpdateExerciseHandler)
-
-    // ROUTINE Handlers
-    mux.HandleFunc("/routines/create", handlers.CreateRoutineHandler)
-    mux.HandleFunc("/routines/list", handlers.GetRoutineListHandler)
-    mux.HandleFunc("/routines/data", handlers.GetRoutineDataHandler)
-    mux.HandleFunc("/routines/delete", handlers.DeleteRoutineHandler)
-    mux.HandleFunc("/routines/update", handlers.UpdateRoutineHandler)
-
-    // WORKOUT Handlers
-    mux.HandleFunc("/workouts/create", handlers.CreateWorkoutHandler)
-    mux.HandleFunc("/workouts/list", handlers.GetWorkoutListHandler)
-    mux.HandleFunc("/workouts/data", handlers.GetWorkoutDataHandler)
-
-    // SESSION Handlers
-    mux.HandleFunc("/session/create", handlers.CreateExerciseHandler)
-    mux.HandleFunc("/session/data", handlers.GetSessionHandler)
-
-    // LOGIN Handlers
+    // PUBLIC
     mux.HandleFunc("/login", handlers.HandleLogin)
-    mux.HandleFunc("/callback", handlers.HandleCallback)
-    
+
+    // USER
+    mux.HandleFunc("/user/create", handlers.CreateUserHandler)
+    mux.Handle("/user/update", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.UpdateUserHandler)))
+    mux.Handle("/user/id", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.GetUserHandler)))
+
+    // EXERCISE
+    mux.Handle("/exercise/create", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.CreateExerciseHandler)))
+    mux.Handle("/exercise/update", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.UpdateExerciseHandler)))
+
+    // ROUTINE
+    mux.Handle("/routines/create", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.CreateRoutineHandler)))
+    mux.Handle("/routines/list", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.GetRoutineListHandler)))
+    mux.Handle("/routines/data", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.GetRoutineDataHandler)))
+    mux.Handle("/routines/delete", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.DeleteRoutineHandler)))
+    mux.Handle("/routines/update", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.UpdateRoutineHandler)))
+
+    // WORKOUT
+    mux.Handle("/workouts/create", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.CreateWorkoutHandler)))
+    mux.Handle("/workouts/list", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.GetWorkoutListHandler)))
+    mux.Handle("/workouts/data", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.GetWorkoutDataHandler)))
+
+    // SESSION
+    mux.Handle("/session/create", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.CreateSessionHandler)))
+    mux.Handle("/session/data", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.GetSessionHandler)))
+
+    // AUTH
+    mux.Handle("/me", middleware.JWTAuthMiddleware(http.HandlerFunc(handlers.HandleMe)))
 }
