9bb21e45ae0548e8d0187b0adb5ef4be6b748907
the-tunnster <therealtarunkrishnan@gmail.com>
2025-06-19 14:21:01 +1000

    Added a couple new handlers for ease of access.

--- internal/handlers/get_handlers.go
+++ internal/handlers/get_handlers.go
@@ -27,6 +27,68 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(user)
 }
 
+func GetExerciseIDHandler(w http.ResponseWriter, r *http.Request) {
+    exerciseNames := r.URL.Query()["exercise_name"]
+    if len(exerciseNames) == 0 {
+        http.Error(w, "Missing exercise_name parameter(s)", http.StatusBadRequest)
+        return
+    }
+
+    exerciseIDs := make([]string, len(exerciseNames))
+    
+    for i, v := range(exerciseNames) {
+        exercise_id, err := database.GetExerciseID(v)
+        if err == nil {
+            exerciseIDs[i] = exercise_id
+        } else {
+            exerciseIDs[i] = primitive.NilObjectID.Hex()
+        }
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(exerciseIDs)
+}
+
+func GetExerciseNameHandler(w http.ResponseWriter, r *http.Request) {
+    exerciseIDs := r.URL.Query()["exercise_id"]
+    if len(exerciseIDs) == 0 {
+        http.Error(w, "Missing exercise_id parameter(s)", http.StatusBadRequest)
+        return
+    }
+
+    exerciseObjIDs := make([]primitive.ObjectID, len(exerciseIDs))
+    exerciseNames := make([]string, len(exerciseIDs))
+
+    for i, v := range(exerciseIDs) {
+        exerciseObjID, err := primitive.ObjectIDFromHex(v)
+        if err != nil {
+            exerciseObjIDs[i] = primitive.ObjectID{}
+        } else {
+            exerciseObjIDs[i] = exerciseObjID
+        }
+    }
+
+    for i, v := range(exerciseObjIDs) {
+        exerciseName, err := database.GetExerciseName(v)
+        if err == nil {
+            exerciseNames[i] = exerciseName
+        } else {
+            exerciseNames[i] = "unknown"
+        }
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(exerciseNames)
+}
+
+func GetExerciseListHandler(w http.ResponseWriter, r *http.Request) {
+    exerciseList := database.GetExerciseList()
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(exerciseList)
+}
+
+
 func GetRoutineListHandler(w http.ResponseWriter, r *http.Request) {
     userID := r.URL.Query().Get("user_id")
     if userID == "" {
