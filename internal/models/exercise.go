dfe628561139f31ddf0902f55557a05ba0d7d7b3
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-03 15:39:13 +1000

    Define ExerciseSets and ExerciseHistory types for managing exercise data and history

--- internal/models/exercise.go
+++ internal/models/exercise.go
@@ -12,3 +12,17 @@ type Exercise struct {
 	Variations      []string           `bson:"variations" json:"variations"`
 	Equipment       []string           `bson:"equipment" json:"equipment"`
 }
+
+type ExerciseSets struct {
+	Date        primitive.DateTime `bson:"date" json:"date"`
+	Equipment   string             `bson:"equipment" json:"equipment"`
+	Variation   string             `bson:"variation" json:"variation"`
+	WorkoutSets []WorkoutSet       `bson:"sets" json:"sets"`
+}
+
+type ExerciseHistory struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	UserID     primitive.ObjectID `bson:"userID" json:"user_id"`
+	ExerciseID primitive.ObjectID `bson:"exerciseID" json:"exercise_id"`
+	Sets       []ExerciseSets     `bson:"exercise_sets" json:"exercise_sets"`
+}
