7133cd4eac2fffe518120e636eeb3768478d075c
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-28 19:16:10 +1000

    Remove muscle fields from Exercise model

--- internal/models/exercise.go
+++ internal/models/exercise.go
@@ -6,9 +6,6 @@ type Exercise struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name            string             `bson:"name" json:"name"`
 	Category        string             `bson:"category" json:"category"`
-	PrimaryMuscle   string             `bson:"primaryMuscle" json:"primary_muscle"`
-	SecondaryMuscle string             `bson:"secondaryMuscle" json:"secondary_muscle"`
-	TertiaryMuscle  string             `bson:"tertiaryMuscle" json:"tertiary_muscle"`
 	Variations      []string           `bson:"variations" json:"variations"`
 	Equipment       []string           `bson:"equipment" json:"equipment"`
 }
