c6899df3db8c3de9a9ba60d0cab063adea5340dd
the-tunnster <therealtarunkrishnan@gmail.com>
2025-08-14 16:05:39 +1000

    Refactor User model for improved readability and maintainability

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,13 +3,15 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Username       string             `bson:"username" json:"username"`
-	Email          string             `bson:"email" json:"email"`
-	Gender         string             `bson:"gender" json:"gender"`
-	DateOfBirth    string             `bson:"dateOfBirth" json:"dateOfBirth"`
-	Height         float64            `bson:"height" json:"height"`
-	Weight         float64            `bson:"weight" json:"weight"`
-	UnitPreference string             `bson:"unitPreference" json:"unitPreference"`
-	ClearanceLevel int                `bson:"clearanceLevel" json:"clearanceLevel"`
+	ID                 primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username           string             `bson:"username" json:"username"`
+	Email              string             `bson:"email" json:"email"`
+	Gender             string             `bson:"gender" json:"gender"`
+	DateOfBirth        string             `bson:"dateOfBirth" json:"dateOfBirth"`
+	Height             float64            `bson:"height" json:"height"`
+	Weight             float64            `bson:"weight" json:"weight"`
+	UnitPreference     string             `bson:"unitPreference" json:"unitPreference"`
+	ClearanceLevel     int                `bson:"clearanceLevel" json:"clearanceLevel"`
+	StravaAccessToken  string             `bson:"stravaAccessToken" json:"stravaAccessToken"`
+	StravaRefreshToken string             `bson:"stravaRefreshToken" json:"stravaRefreshToken"`
 }
